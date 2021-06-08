2c91c4419ba2b8c26f673464a1a523639e477033
ohad92 <73739848+ohad92@users.noreply.github.com>
2021-06-08 12:26:45 +0300

    check

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ Vue.config.productionTip = false;
 
 const shared_data = {
   // username: localStorage.username,
-  username: "hilla",
+  username: "hilla1",
   login(username) {
     localStorage.setItem("username", username);
     this.username = username;
